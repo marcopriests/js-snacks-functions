e1ea922d29163edb1a54593a416a96a265798498
marcopriests <prtmrc98@gmail.com>
2025-03-24 15:51:35 +0100

    [UPD] 04 - 1 fatto

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,23 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+//funzione normale
+function vowelsCounter(string) {
+    let counter = 0
+
+    for (let i = 0; i < string.length; i++) {
+        
+        if (string[i].toLowerCase() === "a" || string[i].toLowerCase() === "e" || string[i].toLowerCase() === "i" || string[i].toLowerCase() === "o" || string[i].toLowerCase() === "u") {
+            counter++
+        }
+        
+    }
+
+    return counter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelsCounter(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
