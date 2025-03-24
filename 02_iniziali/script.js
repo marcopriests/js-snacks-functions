427942da131bf9669a7c541ade20ccc89a2c4887
marcopriests <prtmrc98@gmail.com>
2025-03-24 15:34:07 +0100

    [UPD] 02 - 1 fatto

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+//funzione normale
+function firstLetters(array) {  
+
+    const firstLettersArr = []
+
+    for (let i = 0; i < array.length; i++) {
+
+        firstLettersArr.push(array[i][0])
+        
+    }
+
+    return firstLettersArr
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetters(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
