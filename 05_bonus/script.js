6d5af572d03e19e167958b31afada69be1814ff9
marcopriests <prtmrc98@gmail.com>
2025-03-24 16:08:31 +0100

    [BONUS] 05 - 1 fatto

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,21 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+//funzione normale
+function greetingsByTime(string) {
+    const d = new Date()
+
+    if (d.getHours <= 13) {
+        return "Buongiorno " + string
+    } else if (d.getHours <= 17) {
+        return "Buon pomeriggio " + string        
+    } else {
+        return "Buonasera " + string
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(greetingsByTime(name))
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
